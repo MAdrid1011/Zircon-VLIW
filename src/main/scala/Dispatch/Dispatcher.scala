86f534318682eba4611f64b1296776ed99c4af28
XWY666 <399036866@qq.com>
2025-10-15 05:42:15 -0400

    10.15，补充仿真环境修改

--- src/main/scala/Dispatch/Dispatcher.scala
+++ src/main/scala/Dispatch/Dispatcher.scala
@@ -29,9 +29,6 @@ class Dispatcher extends Module {
     }
 
     val funcMaskTable = Wire(Vec(nfch + 1, Vec(nfch, UInt(nfuncUnit.W))))
-    val funcIdTable = Wire(Vec(nfch, UInt(nfuncUnit.W)))
-    val ftePkgTable = Wire(Vec(nfch, (new BackendPackage)))
-
 
     funcMaskTable(0) := io.func
 
@@ -39,42 +36,35 @@ class Dispatcher extends Module {
     for (stage <- 0 until nfch) {
         // 寻找可用FU最少的指令
         // 统计当前所有指令可用FU数
-        // TODO
         val currentPopCntList = funcMaskTable(stage).map(PopCount(_))
         
         // 选出可用FU最少的指令和索引
-        val (min_pop, min_idx) = currentPopCntList.zipWithIndex
-            .map { case (p, i) => (Mux(p === 0.U, (nfuncUnit + 1).U, p), (1<<i).U) } // 忽略已无请求的指令
-            // reduce-reduceTree
-            .reduce[(UInt, UInt)] { case ((p1, i1), (p2, i2)) =>
+        val minIdx = currentPopCntList.zipWithIndex
+        .map { case (p, i) => (Mux(p === 0.U, (nfuncUnit + 1).U, p), i.U) } // 忽略已无请求的指令
+        // reduce-reduceTree
+        .reduce[(UInt, UInt)] { case ((p1, i1), (p2, i2)) =>
             val choose_p1 = p1 < p2
             val min_p_result = Mux(choose_p1, p1, p2)
             val min_i_result = Mux(choose_p1, i1, i2)
-            (min_p_result, min_i_result)
-        }
+            (min_p_result, min_i_result)  // 只保留最小 popcount 和相应索引
+        }._2  // 只返回 min_idx (即索引)
 
         // 分配FU
         // log2OH PriorityMux
         // 对可用FU最少的一条指令掩码进行优先级编码
-        val fuAllocOh = PriorityEncoderOH(Mux1H(min_idx,funcMaskTable(stage)))
-        funcIdTable(stage) := fuAllocOh
-        ftePkgTable(stage) := Mux1H(min_idx,io.ftePkg.map(_.bits)) 
-        // io.bkePkg(fuIdx).valid := true.B
-        // io.bkePkg(fuIdx).bits  := io.ftePkg(min_idx).bits
+        val fuAllocOh = PriorityEncoderOH(funcMaskTable(stage)(minIdx))
+        val fuIdx = OHToUInt(fuAllocOh)
+
+        io.bkePkg(fuIdx).valid := true.B
+        io.bkePkg(fuIdx).bits  := io.ftePkg(minIdx).bits
 
         // 可用FU矩阵更新
         for (instr_idx <- 0 until nfch) {
             val Mask = ~fuAllocOh // 掩码，清除已被占用的FU
             val current_request = funcMaskTable(stage)(instr_idx)
-            funcMaskTable(stage + 1)(instr_idx) := Mux(min_idx(instr_idx), 0.U,current_request & Mask) 
+            funcMaskTable(stage + 1)(instr_idx) := Mux(instr_idx.U === minIdx, 0.U,current_request & Mask) 
         }
     }
-    val funcIdTableTrans = Transpose(funcIdTable)
-    io.bkePkg.zipWithIndex.foreach{case (p , i) => 
-        p.valid := true.B
-        p.bits := Mux1H(funcIdTableTrans(i),ftePkgTable)
-    }
-
     // --- 反压信号 ---
     io.ftePkg.foreach { ftePkg => ftePkg.ready := bkePkgAllReady }
 }
